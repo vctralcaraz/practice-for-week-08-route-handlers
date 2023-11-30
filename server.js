02ec044d39da036024365ba78f5e336a511580d7
Victor Alcaraz <vctralcaraz@gmail.com>
2023-11-29 21:25:47 -0800

    complete

--- server.js
+++ server.js
@@ -34,6 +34,56 @@ const server = http.createServer((req, res) => {
     // Do not edit above this line
 
     // define route handlers here
+    if(req.method === 'GET' && req.url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Dog Club');
+    }
+    if(req.method === 'GET' && req.url ==='/dogs') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Dogs index');
+    }
+    if(req.method === 'POST' && req.url === '/dogs') {
+      res.statusCode = 302;
+      res.setHeader('Location', '/dogs/' + getNewDogId());
+
+      return res.end();
+    }
+    if(req.url.startsWith('/dogs')) {
+      const subs = req.url.split('/');
+
+      if(req.method === 'GET') {
+        if(subs[2] === 'new') {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+  
+          return res.end('Dog create form page');
+        }
+        if(subs.length === 4 && subs[3] === 'edit') {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+  
+          return res.end(`Dog edit form page for ${subs[2]}`);
+        }
+        if(subs.length === 3) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+  
+          return res.end(`Dog details for ${subs[2]}`);
+        }
+      }
+      if(req.method === 'POST') {
+        if(subs.length === 3) {
+          res.statusCode = 302;
+          res.setHeader('Location', '/dogs/' + subs[2]);
+
+          return res.end();
+        }
+      }
+    }
 
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
